a8b10fee699beb4343a7195083178ecbe782ab4c
unknown <egapatria162@gmail.com>
2024-10-09 18:36:11 +0800

    css

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/auditor', function () {
-    return view('pages.auditor', ['type_menu' => 'auditor']);
-});
-
-Route::get('/formaudit', function () {
-    return view('pages.formaudit', ['type_menu' => 'formaudit']);
-});
-
-Route::get('/auth-login', function () {
-    return view('pages.auth-login', ['type_menu' => 'auth']);
-});
-
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('Test',function(){
-    return "Testing route";
-});
 
 Route::get('/', [UserController::class, 'login'])->name('login');
 Route::post('/', [UserController::class, 'loginAction'])->name('login.action');
@@ -42,6 +19,13 @@ Route::middleware('auth')->group(function () {
     
     Route::get('/audit-internal', function () {
         return view('pages.form-audit-internal', ['type_menu' => 'audit-internal']);
+    });
+    Route::get('/auditor', function () {
+        return view('pages.auditor', ['type_menu' => 'auditor']);
+    });
+    
+    Route::get('/formaudit', function () {
+        return view('pages.formaudit', ['type_menu' => 'formaudit']);
     });
 });
 
