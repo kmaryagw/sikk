ca99425f241d12715b7c5826e39e68c60938470e
unknown <egapatria162@gmail.com>
2024-10-10 14:36:53 +0800

    crud user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
 {
@@ -23,6 +24,7 @@ class UserController extends Controller
         
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $request->session()->regenerate();
+            Alert::success('Sukses', 'Selamat Datang');
             return redirect()->route('pages.dashboard');
         }
 
@@ -81,7 +83,53 @@ class UserController extends Controller
         $user->password = Hash::make($request->password);
         
         $user->save();
+
+        Alert::success('Sukses', 'Data Berhasil Ditambah');
     
-        return redirect()->route('user')->with(['message' => 'Data Berhasil Ditambah']);
+        return redirect()->route('user.index');
+    }
+
+    public function edit(user $user)
+    {
+        $title = 'Ubah User';
+        $levels = ['admin','prodi','unit kerja'];
+        return view('pages.edit-user', [
+            'title' => $title,
+            'levels' => $levels,
+            'user' => $user,
+            'type_menu' => 'masterdata',
+        ]);
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $request->validate([
+            'nama_user' => 'required',
+            'alamat' => 'required', 
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        if (User::where('email', $request->email)->where('id_user', '<>', $user->id_user)->first()) 
+            return back()->withErrors(['email' => 'Email sudah terdaftar']);
+    
+        $user->nama_user = $request->nama_user;
+        $user->email = $request->email;
+        $user->alamat = $request->alamat;
+        $user->level = $request->level;
+        if ($request->password)
+            $user->password = Hash::make($request->password);
+        $user->save();
+
+        Alert::success('Sukses', 'Data Berhasil Diubah');
+
+        return redirect()->route('user.index');
+    }
+    
+    public function destroy(user $user)
+    {
+        $user->delete();
+        Alert::success('Sukses', 'Data Berhasil Dihapus');
+        return redirect()->route('user.index');
     }
 }
