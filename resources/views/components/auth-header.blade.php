78e01c2ca1bf5db18b8d8c48d8defad7d9ec4614
I Gusti Agung Raditya Gotama <gustiagung080804@gmail.com>
2024-10-18 11:50:25 +0800

    menambah css

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,5 +1,5 @@
 <div class="login-brand">
-    <img src="{{ asset('img/instiki-logo.png') }}"
+    <img src="{{ asset('img/Vertical-Logo.png') }}"
         alt="logo"
         width="100"
         class="shadow-light rounded-circle">
