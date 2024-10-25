963a8b1794d03a94d249543ec749f71d42a13d54
unknown <egapatria162@gmail.com>
2024-10-25 15:20:38 +0800

    fix database

--- database/migrations/2024_10_25_070032_create_rencana_kerja_realisasi_table.php
+++ database/migrations/2024_10_25_070032_create_rencana_kerja_realisasi_table.php
@@ -11,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('monitoring', function (Blueprint $table) {
-            $table->string('mtg_id', 50)->primary();  
-            $table->string('pm_id', 50);
-            $table->string('mtg_capaian', 255);
-            $table->string('mtg_kondisi', 255);
-            $table->string('mtg_kendala', 255);
-            $table->string('mtg_tindak_lanjut', 255);
-            $table->date('mtg_tindak_lanjut_tanggal');  
-            $table->string('mtg_bukti', 255);
+        Schema::create('rencana_kerja_realisasi', function (Blueprint $table) {
+            $table->string('rkr_id', 50)->primary();  
             $table->string('rk_id', 50);
+            $table->string('pm_id', 50);
+            $table->string('rkr_url', 255);
+            $table->string('rkr_file', 255);
+            $table->integer('rkr_capaian');
+            $table->datetime('rkr_tanggal');
             $table->foreign('pm_id')->references('pm_id')->on('periode_monev')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('rk_id')->references('rk_id')->on('rencana_kerja')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
@@ -32,13 +30,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-
-        Schema::table('monitoring', function (Blueprint $table) {
+        Schema::table('rencana_kerja_realisasi', function (Blueprint $table) {
             
             $table->dropForeign(['pm_id']);
             $table->dropForeign(['rk_id']);
             
         });
-        Schema::dropIfExists('monitoring');
+        Schema::dropIfExists('rencana_kerja_realisasi');
     }
 };
