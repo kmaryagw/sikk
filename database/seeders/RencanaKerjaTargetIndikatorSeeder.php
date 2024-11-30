1d175b12b10d1e259f764e0afdef94c1a454367d
I Gusti Agung Raditya Gotama <gustiagung080804@gmail.com>
2024-11-30 16:12:56 +0800

    menambahkan monitoring

--- database/seeders/RencanaKerjaTargetIndikatorSeeder.php
+++ database/seeders/RencanaKerjaTargetIndikatorSeeder.php
@@ -14,7 +14,9 @@ class RencanaKerjaTargetIndikatorSeeder extends Seeder
     public function run(): void
     {
         DB::table('rencana_kerja_target_indikator')->insert([
-            
-            ]);
+            'rkti_id' => '1',
+            'rk_id' => 'RK1D8AF05A0CCC61BA70294829D2313168',
+            'ti_id' => 'TC6B8CE3B7A60BE6057CC704E3B498C96A',
+        ]);
     }
 }
